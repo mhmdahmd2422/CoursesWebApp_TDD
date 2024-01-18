dc39e36a2e17f48119365f77c80911b775a8e97f
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-01-18 02:59:42 +0200

    implement user dashboard page show purchased courses

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -58,4 +60,11 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'purchased_courses')
+            ->withTimestamps()
+            ->orderByDesc('pivot_created_at');
+    }
 }
